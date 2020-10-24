0824ab73c367a98f97fd012ca33cae4647b73b9c
afrokingstan <51683427+afrokingstan@users.noreply.github.com>
2020-10-24 07:54:30 +0100

    completed homepage and css

--- design_lab/settings.py
+++ design_lab/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'design_lab.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -150,3 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
