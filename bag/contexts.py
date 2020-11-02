e3a2db02919be2f888cf62c395aeeeb5c9f4488a
afrokingstan <51683427+afrokingstan@users.noreply.github.com>
2020-11-02 05:38:12 +0000

    Finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,15 +12,27 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
     vat = 0
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_QUOTE_THRESHOLD:
         quote = total * Decimal(settings.STANDARD_QUOTE_PERCENTAGE / 100)
